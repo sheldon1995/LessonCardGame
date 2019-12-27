09ec99b2a72634a662b684b7a03e1f234d540428
Sheldon1995 <sheldon.wentao@gmail.com>
2019-12-27 12:53:23 -0500

    Card Game
    Simple card game for beginners.
    By Chris.

--- LessonCardGame/ViewController.swift
+++ LessonCardGame/ViewController.swift
@@ -10,11 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leftImageView: UIImageView!
+    
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    @IBOutlet weak var leftScoreLabel: UILabel!
+    
+    @IBOutlet weak var rightScoreLabel: UILabel!
+    
+    var leftScore = 0
+    var rightScore = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func DealTapped(_ sender: Any) {
+        
+        // Randomize some numbers
+        let leftrandomNumber = Int.random(in: 2...14)
+        
+        let rightrandomNumber = Int.random(in: 2...14)
+        
+        leftImageView.image = UIImage(named: "card\(leftrandomNumber)")
+        rightImageView.image = UIImage(named: "card\(rightrandomNumber)")
+        
+        // Compare random numbers
+        if(leftrandomNumber > rightrandomNumber){
+            // Left side wins
+            leftScore += 1
+            leftScoreLabel.text = String(leftScore)
+        }
+        else if(leftrandomNumber<rightrandomNumber){
+            // Right side wins
+            rightScore += 1
+            rightScoreLabel.text = String(rightScore)
+        }
+    }
 }
 
